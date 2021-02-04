c7c937f2d184f56fb7747fef70e99d087fa2ce83
bond0887 <jindalsuraj45@gmail.com>
2021-02-04 11:01:24 +0530

    adding port

--- script.js
+++ script.js
@@ -110,6 +110,6 @@ app.put('/images',(req,res)=>{
 
 
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`)
 })
