c2b762b03d657d051ba0c69e2fc2faa5938b7787
bond0887 <jindalsuraj45@gmail.com>
2021-02-04 13:10:37 +0530

    fix

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var corsOptions = {
 const db = knex({
 	client: 'pg',
 	connection:{
-		connectionString: 'process.env.DATABASE_URL',
+		connectionString:'process.env.DATABASE_URL',
 		ssl: true,
 	}
 });
